9befe6389b9090a8ae1d8422fd01ac7e39222138
Jon Dewi Jones <jondewijones@gmail.com>
2017-11-09 10:47:52 +0000

    Vendor datasetEditionsList page model

---
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/datasetEditionsList/model.go
@@ -0,0 +1,18 @@
+package datasetEditionsList
+
+import (
+	"github.com/ONSdigital/dp-frontend-models/model"
+	"github.com/ONSdigital/dp-frontend-models/model/datasetLandingPageFilterable"
+)
+
+type Page struct {
+	model.Page
+	datasetLandingPageFilterable.DatasetLandingPage
+	ContactDetails model.ContactDetails `json:"contact_details"`
+	Editions       []Edition            `json:"editions"`
+}
+
+type Edition struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
+}
