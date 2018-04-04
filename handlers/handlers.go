ba2b47c061274e83753fb5a7de4bb91d3631e92f
mattrout92 <matt_rout92@live.co.uk>
2018-04-04 09:03:29 +0100

    Assign florence token to a variable

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -72,10 +72,11 @@ func setStatusCode(req *http.Request, w http.ResponseWriter, err error) {
 func setAuthTokenIfRequired(req *http.Request) ([]dataset.Config, []filter.Config) {
 	var datasetConfig []dataset.Config
 	var filterConfig []filter.Config
-	if len(req.Header.Get("X-Florence-Token")) > 0 {
+	florenceToken := req.Header.Get("X-Florence-Token")
+	if len(florenceToken) > 0 {
 		cfg := config.Get()
-		datasetConfig = append(datasetConfig, dataset.Config{InternalToken: cfg.DatasetAPIAuthToken, FlorenceToken: req.Header.Get("X-Florence-Token")})
-		filterConfig = append(filterConfig, filter.Config{InternalToken: cfg.FilterAPIAuthToken, FlorenceToken: req.Header.Get("X-Florence-Token")})
+		datasetConfig = append(datasetConfig, dataset.Config{InternalToken: cfg.DatasetAPIAuthToken, FlorenceToken: florenceToken})
+		filterConfig = append(filterConfig, filter.Config{InternalToken: cfg.FilterAPIAuthToken, FlorenceToken: florenceToken})
 	}
 	return datasetConfig, filterConfig
 }
