73f43e2e0bee06a2336bf44d1a05e67a9c60970b
Crispioso <crispin.merriman@gmail.com>
2018-05-21 17:27:06 +0100

    Get service from URL query for feedback

--- vendor/github.com/ONSdigital/dp-frontend-models/model/feedback/feedback.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/feedback/feedback.go
@@ -5,11 +5,12 @@ import "github.com/ONSdigital/dp-frontend-models/model"
 // Page contains data reused for feedback model
 type Page struct {
 	model.Page
-	Radio       string `json:"radio"`
-	Purpose     string `json:"purpose"`
-	Feedback    string `json:"feedback"`
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	ErrorType   string `json:"error_type"`
-	PreviousURL string `json:"previous_url"`
+	Radio              string `json:"radio"`
+	Purpose            string `json:"purpose"`
+	Feedback           string `json:"feedback"`
+	Name               string `json:"name"`
+	Email              string `json:"email"`
+	ErrorType          string `json:"error_type"`
+	PreviousURL        string `json:"previous_url"`
+	ServiceDescription string `json:"service_description"`
 }
