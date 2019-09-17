bb20d4b83d158a3e089fb8ada2e6e85969626bca
Jon Dewi Jones <jondewijones@gmail.com>
2019-09-17 17:32:34 +0100

    Clean up vendored dependacies

--- vendor/github.com/ONSdigital/go-ns/rchttp/client.go
+++ vendor/github.com/ONSdigital/go-ns/rchttp/client.go
@@ -155,8 +155,14 @@ func (c *Client) Do(ctx context.Context, req *http.Request) (*http.Response, err
 		return nil, err
 	}
 
-	if c.ExponentialBackoff && resp.StatusCode >= http.StatusInternalServerError {
-		return c.backoff(doer, err, ctx, c.HTTPClient, req, errors.New("Bad server status"))
+	if c.ExponentialBackoff {
+		if resp.StatusCode >= http.StatusInternalServerError {
+			return c.backoff(doer, err, ctx, c.HTTPClient, req, errors.New("Bad server status"))
+		}
+
+		if resp.StatusCode == http.StatusConflict {
+			return c.backoff(doer, err, ctx, c.HTTPClient, req, errors.New("Conflict - request could not be completed due to a conflict with the current state of the target resource"))
+		}
 	}
 
 	return resp, err
@@ -228,7 +234,7 @@ func (c *Client) backoff(f func(...interface{}) (*http.Response, error), retryEr
 			err = args[0].(context.Context).Err()
 			return
 		}
-		if err == nil && resp.StatusCode < http.StatusInternalServerError {
+		if err == nil && resp.StatusCode < http.StatusInternalServerError && resp.StatusCode != http.StatusConflict {
 			return
 		}
 	}
