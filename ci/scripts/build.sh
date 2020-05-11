5cfa3ffcf832feb544bd2547542a8d3962e4543e
Dave Llewellyn <439837@gmail.com>
2020-05-11 11:41:15 +0100

    Testing out changes

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -eux
 
-cwd=$(pwd)
+export BINPATH=$(pwd)/build
+export cwd=$(pwd)
 
 pushd $cwd/dp-frontend-dataset-controller
-  make build && cp build/dp-frontend-dataset-controller $cwd/build
-  cp Dockerfile.concourse $cwd/build
+  BINPATH=$BINPATH make build
+  cp Dockerfile.concourse $BINPATH
 popd
