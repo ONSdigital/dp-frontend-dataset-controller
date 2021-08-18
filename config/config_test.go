f4366bc15ab504e3e8e81938027f7471279c08dc
Ravi <ravipradhan@protonmail.com>
2021-08-18 09:50:56 +0100

    fix config test

--- config/config_test.go
+++ config/config_test.go
@@ -20,7 +20,7 @@ func TestConfig(t *testing.T) {
 			})
 
 			Convey("That the values should be set to the expected defaults", func() {
-				So(cfg.BindAddr, ShouldEqual, ":20200")
+				So(cfg.BindAddr, ShouldEqual, "localhost:20200")
 				So(cfg.Debug, ShouldBeFalse)
 				So(cfg.EnableCensusPages, ShouldBeFalse)
 				So(cfg.APIRouterURL, ShouldEqual, "http://localhost:23200/v1")
