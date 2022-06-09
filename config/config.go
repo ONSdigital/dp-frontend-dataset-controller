60a0fa6d240237ed2b6cbf364eda6556f1d19e2f
NickL <19624419+mr-nick17@users.noreply.github.com>
2022-06-09 10:58:00 +0100

    5695 share dataset link; 5694 remove margin bottom

--- config/config.go
+++ config/config.go
@@ -36,7 +36,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/778ac19"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/6ee63c5"
 	}
 
 	return cfg, nil
