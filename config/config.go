f6db59881ef667c44cdd09d0acf0adc690d3ecc1
Liam Lewis <liamjlewis@gmail.com>
2022-10-27 15:03:03 +0100

    updated DS hash

--- config/config.go
+++ config/config.go
@@ -38,7 +38,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/a2d0e97"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/c4758e3"
 	}
 
 	return cfg, nil
