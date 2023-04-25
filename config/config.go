8a81039e588fbe605b4d40791a5e847a266d91e1
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2023-04-25 15:03:54 +0100

    Update config docs

--- config/config.go
+++ config/config.go
@@ -10,21 +10,21 @@ var cfg *Config
 
 // Config represents service configuration for dp-frontend-dataset-controller
 type Config struct {
-	BindAddr                      string        `envconfig:"BIND_ADDR"`
-	Debug                         bool          `envconfig:"DEBUG"`
-	EnableMultivariate            bool          `envconfig:"ENABLE_MULTIVARIATE"`
 	APIRouterURL                  string        `envconfig:"API_ROUTER_URL"`
-	SiteDomain                    string        `envconfig:"SITE_DOMAIN"`
-	PatternLibraryAssetsPath      string        `envconfig:"PATTERN_LIBRARY_ASSETS_PATH"`
-	SupportedLanguages            []string      `envconfig:"SUPPORTED_LANGUAGES"`
-	DownloadServiceURL            string        `envconfig:"DOWNLOAD_SERVICE_URL"`
-	GracefulShutdownTimeout       time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
-	HealthCheckInterval           time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
-	HealthCheckCriticalTimeout    time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
-	EnableProfiler                bool          `envconfig:"ENABLE_PROFILER"`
-	PprofToken                    string        `envconfig:"PPROF_TOKEN" json:"-"`
+	BindAddr                      string        `envconfig:"BIND_ADDR"`
 	CacheNavigationUpdateInterval time.Duration `envconfig:"CACHE_NAVIGATION_UPDATE_INTERVAL"`
+	Debug                         bool          `envconfig:"DEBUG"`
+	DownloadServiceURL            string        `envconfig:"DOWNLOAD_SERVICE_URL"`
+	EnableMultivariate            bool          `envconfig:"ENABLE_MULTIVARIATE"`
 	EnableNewNavBar               bool          `envconfig:"ENABLE_NEW_NAV_BAR"`
+	EnableProfiler                bool          `envconfig:"ENABLE_PROFILER"`
+	GracefulShutdownTimeout       time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	HealthCheckCriticalTimeout    time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
+	HealthCheckInterval           time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
+	PatternLibraryAssetsPath      string        `envconfig:"PATTERN_LIBRARY_ASSETS_PATH"`
+	PprofToken                    string        `envconfig:"PPROF_TOKEN" json:"-"`
+	SiteDomain                    string        `envconfig:"SITE_DOMAIN"`
+	SupportedLanguages            []string      `envconfig:"SUPPORTED_LANGUAGES"`
 }
 
 // Get returns the default config with any modifications through environment
@@ -50,19 +50,19 @@ func get() (*Config, error) {
 	}
 
 	cfg = &Config{
-		BindAddr:                      "localhost:20200",
-		Debug:                         false,
-		EnableMultivariate:            false,
 		APIRouterURL:                  "http://localhost:23200/v1",
+		BindAddr:                      "localhost:20200",
+		CacheNavigationUpdateInterval: 10 * time.Second,
+		Debug:                         false,
 		DownloadServiceURL:            "http://localhost:23600",
+		EnableMultivariate:            false,
+		EnableNewNavBar:               false,
+		EnableProfiler:                false,
+		GracefulShutdownTimeout:       5 * time.Second,
+		HealthCheckCriticalTimeout:    90 * time.Second,
+		HealthCheckInterval:           30 * time.Second,
 		SiteDomain:                    "localhost",
 		SupportedLanguages:            []string{"en", "cy"},
-		GracefulShutdownTimeout:       5 * time.Second,
-		HealthCheckInterval:           30 * time.Second,
-		HealthCheckCriticalTimeout:    90 * time.Second,
-		EnableProfiler:                false,
-		CacheNavigationUpdateInterval: 10 * time.Second,
-		EnableNewNavBar:               false,
 	}
 
 	return cfg, envconfig.Process("", cfg)
