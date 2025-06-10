2597e39158fe033fedc87b6a58be2ea047aad9f0
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-06-10 16:26:53 +0100

    bump dp-design-system and sixteens versions

--- config/config_test.go
+++ config/config_test.go
@@ -31,7 +31,7 @@ func TestConfig(t *testing.T) {
 				So(cfg.HealthCheckCriticalTimeout, ShouldEqual, 90*time.Second)
 				So(cfg.IsPublishing, ShouldEqual, false)
 				So(cfg.EnableProfiler, ShouldBeFalse)
-				So(cfg.PatternLibraryAssetsPath, ShouldEqual, "//cdn.ons.gov.uk/dp-design-system/28d6b95")
+				So(cfg.PatternLibraryAssetsPath, ShouldEqual, "//cdn.ons.gov.uk/dp-design-system/f3e1909")
 			})
 		})
 	})
