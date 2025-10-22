df81b9094575416c5ec15a9196d332ce385b0a81
jondewijones <jondewijones@gmail.com>
2025-10-22 09:49:44 +0100

    add basic approve handler

---
+++ handlers/approve.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	dpDatasetApiSdk "github.com/ONSdigital/dp-dataset-api/sdk"
+	"github.com/ONSdigital/dp-frontend-dataset-controller/config"
+	"github.com/ONSdigital/dp-net/v3/handlers"
+	"github.com/ONSdigital/log.go/v2/log"
+	"github.com/gorilla/mux"
+)
+
+// ApproveDatasetVersion handles the approve button click on static dataset version pages
+func ApproveDatasetVersion(dc DatasetAPISdkClient, cfg config.Config) http.HandlerFunc {
+	return handlers.ControllerHandler(func(w http.ResponseWriter, req *http.Request, lang, collectionID, userAccessToken string) {
+		approveDatasetVersion(w, req, dc, collectionID, userAccessToken, lang)
+	})
+}
+
+func approveDatasetVersion(w http.ResponseWriter, req *http.Request, dc DatasetAPISdkClient, collectionID, userAccessToken, lang string) {
+	ctx := req.Context()
+
+	vars := mux.Vars(req)
+	datasetID := vars["datasetID"]
+	editionID := vars["editionID"]
+	versionID := vars["versionID"]
+
+	headers := dpDatasetApiSdk.Headers{
+		CollectionID:         collectionID,
+		DownloadServiceToken: "",
+		ServiceToken:         "",
+		UserAccessToken:      userAccessToken,
+	}
+
+	err := dc.PutVersionState(ctx, headers, datasetID, editionID, versionID, "approved")
+	if err != nil {
+		log.Error(ctx, "dataset version approval failed", err)
+	}
+
+	uri := fmt.Sprintf("/datasets/%s/editions/%s/versions/%s", datasetID, editionID, versionID)
+	http.Redirect(w, req, uri, http.StatusTemporaryRedirect)
+}
