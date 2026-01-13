072fae9cde45b41fd320380d2696f6232bd71ed0
fran moore <franmoore05@gmail.com>
2026-01-13 11:24:07 +0000

    User bearer prefix from dp-net

--- permissions/permissions_check.go
+++ permissions/permissions_check.go
@@ -7,11 +7,13 @@ import (
 
 	"strings"
 
+	"github.com/ONSdigital/dp-net/v3/request"
+
 	auth "github.com/ONSdigital/dp-authorisation/v2/authorisation"
 )
 
 func CheckIsAdmin(ctx context.Context, token string, authorisation auth.Middleware) (bool, error) {
-	userToken := strings.ReplaceAll(token, "Bearer ", "")
+	userToken := strings.ReplaceAll(token, request.BearerPrefix, "")
 	entityData, err := authorisation.Parse(userToken)
 	if err != nil {
 		return false, fmt.Errorf("check admin: failed to parse user JWT token: %w", err)
