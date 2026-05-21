34e0210d8d00a5d00626f55e2aa59fea4c6bd19a
aryan-patel <aryanpatel1771@gmail.com>
2026-05-21 12:32:36 +0100

    add missing version link error mapping

--- handlers/errors.go
+++ handlers/errors.go
@@ -7,12 +7,14 @@ import (
 
 // List of errors used within the handlers package
 var (
-	errDatasetTypeNotSupported = errors.New("dataset type is not supported")
-	errDatasetTopicMismatch    = errors.New("dataset topic does not match topic in URL")
+	errDatasetTypeNotSupported  = errors.New("dataset type is not supported")
+	errDatasetTopicMismatch     = errors.New("dataset topic does not match topic in URL")
+	errMissingLatestVersionLink = errors.New("latest version link is missing from dataset API response")
 )
 
 // Map of errors to HTTP status codes
 var errorToStatusCodeMap = map[error]int{
-	errDatasetTypeNotSupported: http.StatusNotFound,
-	errDatasetTopicMismatch:    http.StatusNotFound,
+	errDatasetTypeNotSupported:  http.StatusNotFound,
+	errDatasetTopicMismatch:     http.StatusNotFound,
+	errMissingLatestVersionLink: http.StatusInternalServerError,
 }
